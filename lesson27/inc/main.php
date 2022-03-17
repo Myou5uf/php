ab73a2279e40b74f9386a58009f6a93a95f25479
Myou5uf <myxamed12l@gmail.com>
2022-03-18 01:45:56 +0700

    Update Lesson 27

--- lesson27/inc/main.php
+++ lesson27/inc/main.php
@@ -1,42 +1,58 @@
 <?php
-require_once('inc/func.php');
 
-echo '<h3>Главная часть</h3>';
+// * Копирования файл в папу file с новым называнием file.txt
+// copy('copy.txt', 'file/file.txt');
 
-// Копирование файла из папки inc/text.txt в папку file c новым названием file.txt
-// copy('inc/text.txt', 'file/file.txt');
+// * file_exists() - Проверяет наличие указанного файла или каталога
+if (file_exists('file/file.txt')) {
+    echo 'Файл существует';
+} else {
+    echo 'Такого файла нет';
+}
 
-// Если файл существует то ...., иначе ...
-// if (file_exists('file/file.txt')) {
-//     echo 'Файл существует';
-// } else {
-//     echo 'Файла нет';
-// }
+echo '<br>';
+// * file_get_contents() - Читает содержимое файла в строку
+// echo nl2br($file = file_get_contents('file/file.txt'));
+// echo $filePhp = file_get_contents('https://www.php.net/');
 
-// $file = file_get_contents('inc/text.txt');
-// echo $file;
-
+// * file_put_contents() - Пишет строку в файл
+// $file1 = file_get_contents('https://www.php.net/manual/ru/function.file-put-contents.php');
 // $file2 = file_get_contents('https://www.php.net/');
-// echo $file2;
-
-// $file = file_get_contents('https://www.php.net/');
-// $file2 = file_get_contents('inc/text.txt');
-// file_put_contents('file/file.txt', $file);
-// добавить к существуещему файлу ещё данные  в конец
+// file_put_contents('file/file.txt', $file1);
 // file_put_contents('file/file.txt', $file2, FILE_APPEND);
 
-
-$file = file('inc/text.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-echo '<pre?>';
-print_r($file);
+// * file() - Читает содержимое файла и помещает его в массив
+$file3 = file('copy.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+echo '<pre>';
+print_r($file3);
 echo '</pre>';
 
-// echo '<br>';
-// if (is_dir('file')) echo "Это папка";
-// else echo "Это не папка";
+// * is_dir() - Определяет, является ли имя файла директорией
+if (is_dir('file')) echo "Это каталог";
+else echo "Это НЕ каталог";
 
-// if (is_dir('.')) echo "Это папка";  // . - это указатель текущей папки, относительно файла main.php
-// else echo "Это не папка";
+echo '<br>';
+// * is_dir() - Определяет, является ли файл обычным файлом
+if (is_file('copy.txt')) echo "Это файл";
+else echo "Это НЕ файл";
 
-// if (is_dir('..')) echo "Это папка"; // .. - это указатель родительской папки папки inc в котром находится файл main.php
-// else echo "Это не папка";
+echo '<br>';
+// * rename() - переименовывает файл или директорию
+rename('copy.txt', 'file/copy.txt');
+
+echo '<br>';
+// * mkdir() - Создает директорию 
+// mkdir('1/2/3', 0777, true);
+
+echo '<br>';
+// * rmdir() - Удаляет директорию
+// mkdir('rmdir');
+// rmdir('rmdir');
+
+echo '<br>';
+// * touch() - Устанавливет время доступа и модификации файла
+touch('file/copy.txt', time() - 3600 * 24);
+
+echo '<br>';
+// * unlink() - Удаляет файл
+unlink('file/file.txt');
